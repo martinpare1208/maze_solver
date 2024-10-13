799e035881f9d41d2cbb9697b2dab38f14a6eba3
martinpare1208 <martinapilado@gmail.com>
2024-10-13 14:08:30 -0400

    Refactored parameters and drawing cells

--- cell.py
+++ cell.py
@@ -2,19 +2,23 @@ from tkinter import Tk, BOTH, Canvas
 from window import *
 
 class Cell:
-    def __init__(self, has_left_wall, has_right_wall, has_top_wall, has_bottom_wall, x1,x2,y1,y2, win):
-        self.has_left_wall = has_left_wall
-        self.has_right_wall = has_right_wall
-        self.has_top_wall = has_top_wall
-        self.has_bottom_wall = has_bottom_wall
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+        
+    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        self._win = win
-
-        
-    def draw(self):
         if self.has_bottom_wall: 
             self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
         if self.has_right_wall:
--- main.py
+++ main.py
@@ -5,8 +5,26 @@ from cell import *
 def main():
     win = Window(800,600)
     red_line = Line(point1=Point(100, 200), point2=Point(300,400))
-    cell_test = Cell(True, True, True, True, 0, 100, 0, 100, win=win)
-    cell_test.draw()
+    # cell_test = Cell(True, True, True, True, 0, 100, 0, 100, win=win)
+    # cell_test.draw()
+    
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+    
+    
     win.draw_line(red_line)
     win.wait_for_close()
 
